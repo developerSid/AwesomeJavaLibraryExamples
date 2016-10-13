3ff618549a79a769d72c459190fe1e9bb6dfe3cb
Gary <developer.sid@gmail.com>
2016-10-12 23:50:27 -0500

    Began work on the Stdlib examples

---
+++ Stdlib/src/main/java/org/devict/jlib/stdlib/ExampleStream.java
@@ -0,0 +1,10 @@
+package org.devict.jlib.stdlib;
+
+/**
+ * Created by Gary on 10/12/16.
+ *
+ * Demonstrate some of the Java 8 streams for handling collections
+ */
+public class ExampleStream
+{
+}
